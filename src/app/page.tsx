3dfedc69784a6e4ffd8647a9b2ad2b2df326fa63
Arjun Nair <arjunrajagopal@gmail.com>
2026-05-01 10:13:24 +0100

    Fix duplicate import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { useState } from "react";
 import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
 import { PainPoints } from "@/components/PainPoints";
